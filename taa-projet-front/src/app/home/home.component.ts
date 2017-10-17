132beeab28ffe26bc664d02b386660ce6e5b0857
masoumiaa <aminesoumiaa@gmail.com>
2017-10-17 14:00:26 +0200

    [Front-End][Home-page] : refactoring

--- taa-projet-front/src/app/home/home.component.ts
+++ taa-projet-front/src/app/home/home.component.ts
@@ -11,24 +11,16 @@ export class HomeComponent  {
     groups: Array<any> = [
         {
             heading: 'Rennes',
-            content: ' Cloudy :('
+            content: ' Cloudy :( | Foot : 7/10 | tennis : 5/10 | velo : 6/10'
         },
         {
             heading: 'Nantes',
-            content: ' Sunny :) '
+            content: ' Sunny :) | Foot : 9/10 | tennis : 9/10 | velo : 9/10'
         },
         {
             heading: 'Brest ',
-            content: 'Windy :| '
+            content: 'Windy :| | Foot : 6/10 | tennis : 4/10 | velo : 4/10'
         }
     ];
 
-    removeAngular2AccordionGroup() {
-        this.groups.splice(1,1);
-    }
-
-    addAngular2AccordionGroup() {
-        let accordionGroupContent = {heading:'Hi New Content !', content:'Content angular 2 accordion '};
-        this.groups.splice(1,0,accordionGroupContent);
-    }
 }
